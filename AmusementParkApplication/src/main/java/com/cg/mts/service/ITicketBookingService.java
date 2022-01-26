7b7808a22d4415ecaa008e0008861777c7fb6040
Mahima S <mahimamudaliar@gmail.com>
2022-01-26 15:24:37 +0530

    Mahima S

--- AmusementParkApplication/src/main/java/com/cg/mts/service/ITicketBookingService.java
+++ AmusementParkApplication/src/main/java/com/cg/mts/service/ITicketBookingService.java
@@ -7,6 +7,7 @@ import com.cg.mts.entities.Ticket;
 public interface ITicketBookingService {
 	
 	public Ticket insertTicketBooking(Ticket ticketBooking);
+	public String name;
 	public Ticket updateTicketBooking(Ticket ticketBooking);
 	public Ticket deleteTicketBooking(int ticketId);
 	public List<Ticket> viewAllTicketsCustomer(int customerId);
